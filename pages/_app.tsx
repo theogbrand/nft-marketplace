dc9fdca60e10290a367da4b420c28c471b8dabec
ongjjbrandon <ongjjbrandon@gmail.com>
2022-01-31 17:05:42 +0800

    frontend twcss setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold underline">NFT Marketplace</p>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
